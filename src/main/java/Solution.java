5cc56cee626b1cd81aef2e0b723c9208de783b61
alek <wait82985@gmail.com>
2023-06-14 22:35:48 +0600

    optimized backtracking solution

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -4,25 +4,23 @@ public class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         List<Integer> currentList = new ArrayList<>();
         List<List<Integer>> resultList = new ArrayList<>();
+        Arrays.sort(candidates);
+        backtrack(candidates, target, 0, currentList, resultList, 0);
 
-        Set<List<Integer>> resultSet = new HashSet<>();
-        backtrack(candidates, target, 0, currentList, resultSet);
-        resultList = new ArrayList<>(resultSet);
         return resultList;
     }
-    private void backtrack(int[] candidates, int target, int sum, List<Integer> currentList, Set<List<Integer>> resultSet){
-        if(sum == target){
-            List<Integer> listToAdd = new ArrayList<>(currentList);
-            Collections.sort(listToAdd);
-            resultSet.add(listToAdd);
+
+    private void backtrack(int[] candidates, int target, int sum, List<Integer> currentList, List<List<Integer>> resultList, int start) {
+        if (sum == target) {
+            resultList.add(new ArrayList<>(currentList));
             return;
         }
-        if(sum > target){
+        if (sum > target) {
             return;
         }
-        for (int i = 0; i < candidates.length; i++) {
+        for (int i = start; i < candidates.length; i++) {
             currentList.add(candidates[i]);
-            backtrack(candidates, target, sum + candidates[i], currentList, resultSet);
+            backtrack(candidates, target, sum + candidates[i], currentList, resultList, i);
             currentList.remove(currentList.size() - 1);
         }
     }
